69d7f8902984c6aa3b73bcf31d9f07f147e80b40
Carson Tang <ctang@fb.com>
2018-05-07 17:14:25 +0800

    move validation to data/

--- conf.py
+++ conf.py
@@ -10,5 +10,5 @@ NUM_CLASSES = 10
 
 DATA_DIR = 'data'
 OW_ULT_CHARGE_SYNTHETIC_TRAIN_DATASET_PKL = os.path.join(DATA_DIR, 'synthetic_train_dataset.pkl')
-OW_ULT_CHARGE_VALID_DATASET_PKL = 'validation_dataset.pkl'
+OW_ULT_CHARGE_VALID_DATASET_PKL = os.path.join(DATA_DIR, 'validation_dataset.pkl')
 OW_ULT_CHARGE_SHEARED_VALID_DATASET_PKL = os.path.join(DATA_DIR, 'valid_straight_dataset.pkl')
